2e781bcf30b5198ebd201d4a7904fae1eb064cef
omerb21 <omerb31@gmail.com>
2025-10-08 19:13:53 +0300

    ניקוי מערכת מקיף: הסרת קבצים מיותרים ואופטימיזציה

--- tests/test_calc_pension.py
+++
@@ -1,6 +0,0 @@
-from app.providers.tax_params import InMemoryTaxParamsProvider
-from app.calculation.pensions import calc_monthly_pension_from_capital
-
-def test_pension_monthly_simple():
-    params = InMemoryTaxParamsProvider().get_params()
-    assert calc_monthly_pension_from_capital(200000.0, params) == 1000.0
